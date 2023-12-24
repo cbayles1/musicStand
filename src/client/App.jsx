8016f02d80e04760c8cc2d018797944ab00956d6
Caleb <clbayles@hotmail.com>
2023-12-23 19:46:27 -0600

    search and new axios system is working

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,11 +6,12 @@ import axios from 'axios';
 // either input field or more complicated dropdown for tags
 
 export default function App() {
-//  const [dataFetchUrl, setDataFetchUrl] = useState('http://localhost:3000/getData');
   const [data, setData] = useState(null);
   const [fetchTrigger, setFetchTrigger] = useState(false);
-
-  const dfq = new DataFetchReq('http://localhost:3000/getData', null, null, null, null);
+  const [sort, setSort] = useState(null);
+  const [searchMethod, setSearchMethod] = useState(null);
+  const [searchValue, setSearchValue] = useState(null);
+  const [tag, setTag] = useState(null);
 
   // Runs on page load
   useEffect(() => {
@@ -34,10 +35,40 @@ export default function App() {
         event.preventDefault();
         setFetchTrigger(true);
       }}>
-        <div className="modifiers">
-        <SortModifier dfq={dfq}/>
-        <SearchModifier dfq={dfq}/>
-        <TagModifier dfq={dfq}/>
+        <div id="modifiers">
+          <span id="sortModifiers">
+            Sort by:
+            {[['id', 'Date Added'], ['title', 'Title'], ['author', 'Author']].map((modifierArr, i) => {
+              const modifier = modifierArr[0];
+              const modifierLabel = modifierArr[1];
+
+              let isDefault = false;
+              if (i == 0) {
+                isDefault = true;
+              }
+              return (<div key={i}>
+                <input name="sort" type="radio"
+                  value={modifier.toLowerCase()}
+                  defaultChecked={isDefault}
+                  onChange={(e) => setSort(e.target.value)}
+                />
+                <label>{modifierLabel}</label>
+              </div>);
+            })}
+          </span>
+          
+          <span id="searchModifiers">
+            <label>Search by:</label><br/>
+            <select name="searchMethod" onChange={(e) => setSearchMethod(e.target.value)}>
+              <option value="title">Title</option>
+              <option value="author">Author</option>
+            </select><br/>
+            <label>Search for:</label><br/>
+            <input name="searchValue" type="text" onChange={(e) => setSearchValue(e.target.value)}></input>
+          </span>
+
+          <span id="tagModifiers">Tag</span>
+
         </div>
         <input type="submit" className="modifierSubmit"/>
       </form>
@@ -48,38 +79,26 @@ export default function App() {
   );
   
   async function fetchData() {
-    await dfq.makeRequest().then((result) => {
+    await makeRequest().then((result) => {
       setData(result);
       setFetchTrigger(false);
     });
   }
-}
 
-class DataFetchReq {
-  constructor(baseUrl, initialSort, initialSearchMethod, initialSearchValue, initialTag) {
-    this.baseUrl = baseUrl;
-    this.sort = initialSort;
-    this.searchMethod = initialSearchMethod;
-    this.searchValue = initialSearchValue;
-    this.tag = initialTag;
-  }
-
-  async makeRequest() {
+  async function makeRequest() {
     let params = {
-      sort: this.sort,
-      tag: this.tag
+      sort: sort,
+      tag: tag
     };
-    if (this.searchMethod && this.searchValue) {
-      params.searchMethod = this.searchMethod;
-      params.searchValue = this.searchValue;
+    if (searchMethod && searchValue) {
+      params.searchMethod = searchMethod;
+      params.searchValue = searchValue;
     } else {
       params.searchMethod = null;
       params.searchValue = null;
     }
-
-    console.log(params);
-
-    const res = await axios.get(this.baseUrl, params);
+  
+    const res = await axios.get('http://localhost:3000/getData', {params: params});
     return res.data;
   }
 }
@@ -104,57 +123,4 @@ function Tune({entry}) {
       </span>
     </Collapsible>
   );
-}
-
-function SortModifier({dfq}) {
-  return (
-    <span className="sortModifiers">
-      Sort by:
-      {[['id', 'Date Added'], ['title', 'Title'], ['author', 'Author']].map((modifierArr, i) => {
-        const modifier = modifierArr[0];
-        const modifierLabel = modifierArr[1];
-
-        let isDefault = false;
-        if (i == 0) {
-          isDefault = true;
-        }
-        return (<div key={i}>
-          <input name="sort" type="radio"
-            value={modifier.toLowerCase()}
-            defaultChecked={isDefault}
-            onChange={(e) => {
-              dfq.sort = e.target.value;
-            }}
-          />
-          <label>{modifierLabel}</label>
-        </div>);
-      })}
-    </span>
-  );
-}
-
-function SearchModifier({dfq}) {
-  /*const [searchMethod, setSearchMethod] = useState(null);
-  const [searchValue, setSearchValue] = useState(null);
-
-  useEffect(() => {
-    dfq.searchMethod = searchMethod;
-    dfq.searchValue = searchValue;
-  }, [searchMethod, searchValue]);*/
-
-  return (
-    <span className="searchModifiers">
-      <label>Search by:</label><br/>
-      <select name="searchMethod" onChange={(e) => dfq.searchMethod = e.target.value}>
-        <option value="title">Title</option>
-        <option value="author">Author</option>
-      </select><br/>
-      <label>Search for:</label><br/>
-      <input name="searchValue" type="text" onChange={(e) => dfq.searchValue = e.target.value}></input>
-    </span>
-  );
-}
-
-function TagModifier({dfq}) {
-  return <div>Hi</div>
 }
